425346b1eb2ba89af3bd26e8196e7dab5c2a041d
gretar <gretar95@gmail.com>
2017-10-10 09:54:52 +0000

    timaverkefni6

--- timaverkefni/timaverkefni6.js
+++ timaverkefni/timaverkefni6.js
@@ -1,23 +1,34 @@
 Vue.component("modal", {
-template: `
-<div class="modal">
-	<div class="modal-background"></div>
-	<div class="modal-content">
-	<!-- Any other Bulma elements you want -->
+	props: ["title"],
+	data: function() {
+		return {
+			isVisible: false
+		};
+	},
+	methods: {
+		showModal: function() {
+			this.isVisible = true;
+		},
+		hideModal: function(){
+			this.isVisible = false;
+		}
+	},
+	template: `
+	<div>
+		<div v-show="isVisible" class="modal is-active">
+			<div class="modal-background" @click="hideModal"></div>
+			<div class="modal-content">
+				<div class="box">
+					<h1 class="title is-4">{{ title }}</h1>
+					<p><slot></slot></p>
+				</div>
+			</div>
+			<button class="modal-close is-large" aria-label="close" @click="hideModal"></button>
+		</div>
+		<button class="button is-primary" @click="showModal">Birta modal</button>
 	</div>
-	<button class="modal-close is-large" aria-label="close"></button>
-</div>
 `
 });
 var app = new Vue({
-	el: "#app",
-	data:{
-
-	},
-	methods: {
-		vMsg: function() {
-			
-		}
-	}
-
+	el: "#app"
 });
