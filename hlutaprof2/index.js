aa90725cf73a0f49954a04d409f09665acd81f8f
gretar <gretar95@gmail.com>
2017-10-25 10:09:52 +0000

    hlutaprof 3 halfnad

--- hlutaprof2/index.js
+++ hlutaprof2/index.js
@@ -59,10 +59,10 @@ Vue.component("eg", {
         	<div class="is-centered">
 		        <figure class="image is-480x320">
 		        	<img :src="mynd">
-		        	</div>
-		        		<p class="has-text-centered is-size-4">{{ nafn }}</p>
-		        	</div>
 		        </figure>
+	        	</div>
+	        		<p class="has-text-centered is-size-4">{{ nafn }}</p>
+	        	</div>
 	        </div>
         </div>
 	`
@@ -98,6 +98,39 @@ Vue.component("formid", {
 			</div>
 		</div>
 	`
+});
+Vue.component("modal", {
+	props: ["title"],
+	data: function() {
+		return {
+			isVisible: false
+		};
+	},
+	methods: {
+		showModal: function() {
+			this.isVisible = true;
+		},
+		hideModal: function(){
+			this.isVisible = false;
+		}
+	},
+	template: `
+	<div>
+		<div v-show="isVisible" class="modal is-active">
+			<div class="modal-background" @click="hideModal"></div>
+			<div class="modal-content">
+				<div class="box">
+					<h1 class="title is-4">{{ title }}</h1>
+					<p><slot></slot></p>
+				</div>
+			</div>
+			<button class="modal-close is-large" aria-label="close" @click="hideModal"></button>
+		</div>
+		<div class="has-text-centered">
+		<button class="button is-primary" @click="showModal">{{ title }}</button>
+		</div>
+	</div>
+`
 });
 var app = new Vue({
 	el: '#app',
