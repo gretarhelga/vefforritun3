cae80becc7d9d0dd61d939ad53a7a76318f2df59
gretar <gretar95@gmail.com>
2017-10-25 07:51:45 +0000

    hlutaprof2 halfnad

--- hlutaprof2/index.js
+++ hlutaprof2/index.js
@@ -1,33 +1,72 @@
 Vue.component("tabs", {
 	props: [],
 	template:`
-		<div class="tabs is-centered">
-			<ul>
-			    <li class="is-active"><a>Ég</a></li>
-			    <li><a>Verkefni</a></li>
-			    <li><a>Hafa samband</a></li>
-			    <li><a>Eitthvað skemmtilegt</a></li>
-		    </ul>
+		<div>
+			<div class="tabs is-centered">
+				<ul>
+				    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+				   		<a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+				    </li>
+			    </ul>
+			</div>
+
+			<div class="tabs-details">
+				<slot></slot>
+			</div>
 		</div>
-	`
+	`,
+	data() {
+		return { tabs: [] };
+	},
+	created() {
+		this.tabs = this.$children;
+	},
+	methods: {
+		selectTab(selectedTab) {
+			this.tabs.forEach(tab => {
+				tab.isActive = (tab.name == selectedTab.name);
+			});
+		}
+	}
 });
-/*Vue.component("eg", {
-	props: ["image", "nafn"],
+
+Vue.component("tab", {
+	props: {
+		name: { required: true},
+		selected: { default: false }
+	},
 	template:`
-	    <div class="card-content">
-	    	<div class="media">
-	        	<div class="is-centered">
-			        <figure class="image is-480x320">
-			        	<img :src="image">
-			        	</div class="is-centered">
-			        		<p>{{ nafn }}</p>
-			        	</div>
-			        </figure>
-	        	</div>
+		<div v-show="isActive"><slot></slot></div>
+	`,
+	data(){
+		return {
+			isActive: false
+		};
+	},
+	computed: {
+		href() {
+			return "#" + this.name.toLowerCase().replace(/ /g, "-");
+		}
+	},
+	mounted() {
+		this.isActive = this.selected;
+	}
+});
+Vue.component("eg", {
+	props: ["mynd", "nafn"],
+	template:`
+		<div>
+        	<div class="is-centered">
+		        <figure class="image is-480x320">
+		        	<img :src="mynd">
+		        	</div>
+		        		<p class="has-text-centered is-size-4">{{ nafn }}</p>
+		        	</div>
+		        </figure>
 	        </div>
         </div>
 	`
-});*/
+});
 Vue.component("message", {
 	props: [],
 	template:`
@@ -38,6 +77,26 @@ Vue.component("message", {
 				</div>
 			</div>
 		</article>
+	`
+});
+Vue.component("formid", {
+	template:`
+		<div>
+			<div class="field">
+				<label class="label">Nafn</label>
+				<div class="control">
+					<input class="input" type="text">
+				</div>
+			</div>
+
+			<div class="field">
+				<label class="label">Skilaboð</label>
+				<div class="control">
+					<textarea class="textarea">
+					</textarea>
+				</div>
+			</div>
+		</div>
 	`
 });
 var app = new Vue({
