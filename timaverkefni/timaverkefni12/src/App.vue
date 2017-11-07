89cc4ec6de3098ebc669ca0d1f363a84736c577e
gretar <gretar95@gmail.com>
2017-11-07 10:10:09 +0000

    timaverk12

---
+++ timaverkefni/timaverkefni12/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div v-for="petrol in petrols">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      petrol: []
+    };
+  },
+  mounted() {
+    var self = this;
+    axios.get("http://apis.is/petrol")
+      .then(function(response) {
+        this.petrol = response.data.results;
+      })
+      .catch(function(errors) {
+        console.log(errors);
+      })
+  }
+}
+
+</script>
+
+<style>
+
+</style>
