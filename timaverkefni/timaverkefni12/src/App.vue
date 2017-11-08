6f949c5b97eb7653cc9424c5f3a8bb1a1567e3b3
gretar <gretar95@gmail.com>
2017-11-08 10:13:02 +0000

    timaverk12

--- timaverkefni/timaverkefni12/src/App.vue
+++ timaverkefni/timaverkefni12/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <div v-for="petrol in petrols">
-      
+    <div v-for="petrol in sortedStations">
+      <div class="box">
+        <h3 class="title is-4">{{ petrol.company }}</h3>
+        <h4 class="title is-6">Staðsetning: {{ petrol.name }}</h4>
+        <p>Bensín verð {{ petrol.bensin95 }}kr.</p>
+        <p>Dísel verð {{ petrol.diesel }}kr.</p>
+      </div>
     </div>
   </div>
 </template>
@@ -13,14 +18,21 @@ export default {
   name: 'app',
   data () {
     return {
-      petrol: []
+      petrols: []
     };
+  },
+  computed: {
+    sortedStations() {
+      return this.petrols.sort(function( a,b) {
+        return a.bensin95 - b.bensin95;
+      });
+    }
   },
   mounted() {
     var self = this;
     axios.get("http://apis.is/petrol")
       .then(function(response) {
-        this.petrol = response.data.results;
+        self.petrols = response.data.results;
       })
       .catch(function(errors) {
         console.log(errors);
@@ -30,6 +42,6 @@ export default {
 
 </script>
 
-<style>
+<style> 
 
 </style>
--- timaverkefni/timaverkefni9/timaverk9/src/App.vue
+++ timaverkefni/timaverkefni9/timaverk9/src/App.vue
@@ -1,25 +1,41 @@
 <template>
   <div id="app">
-    <box>Texti i box</box> 
-    <box>Texti i öðru boxi</box> 
-    <box>Texti i enn öðru boxi</box> 
+    <div v-for="concert in concerts">
+      <h3>{{ concert.eventdateName }}</h3>
+      <h5 class="subtitle">{{ concert.eventHallName }}</h5>
+      <img :src="consert.imageSource">
+    </div>
   </div>
 </template>
 
 <script>
-import Box from "./components/Box.vue";
-
-export default {
-  name: 'app',
-  components: { Box },
-  data () {
-    return {
-
+import axios from "axios";
+  export default {
+    name: 'app',
+    data () {
+      return {
+        concerts: []
+      };
+    },
+    mounted() {
+      var self = this;
+      axios.get("http://api.is/concerts")
+      .then(function(response) {
+        this.concerts = response.data.results;
+      })
+      .catch(function(errors) {
+        console.log(errors);
+      })
     }
   }
-}
+
 </script>
 
 <style>
-
+  body {
+    padding:20px;
+  }
+  .subtitle {
+    color: #d3d3d3;
+  }
 </style>
