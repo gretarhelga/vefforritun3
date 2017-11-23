fa3324628c25b380ce4d14c966127ee0d8259112
gretar <gretar95@gmail.com>
2017-11-23 10:07:21 +0000

    heimaverk3

--- timaverkefni/timaverkefni12/src/App.vue
+++ timaverkefni/timaverkefni12/src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
     <div class="column is-6 is-offset-3">
-      <input type="radio" id="orkan" v-model="stations">
-      <label for="orkan">Orkan</label>
+
+      <input type="radio" id="Allt" value="Allt" v-model="stations">
+      <label for="Allt">Allt</label>
+
+      <input type="radio" id="Atlantsolía" value="Atlantsolía" v-model="stations">
+      <label for="Atlantsolía">Atlantsolía</label>
+
+      <input type="radio" id="Costco Iceland" value="Costco Iceland" v-model="stations">
+      <label for="Costco Iceland">Costco Iceland</label>
+
+      <input type="radio" id="Dælan" value="Dælan" v-model="stations">
+      <label for="Dælan">Dælan</label>
+
+      <input type="radio" id="N1" value="N1" v-model="stations">
+      <label for="N1">N1</label>
+
+      <input type="radio" id="Olís" value="Olís" v-model="stations">
+      <label for="Olís">Olís</label>
+
+      <input type="radio" id="Orkan" value="Orkan" v-model="stations">
+      <label for="Orkan">Orkan</label>
+
+      <input type="radio" id="Orkan X" value="Orkan X" v-model="stations">
+      <label for="Orkan X">Orkan X</label>
+
+      <input type="radio" id="ÓB" value="ÓB" v-model="stations">
+      <label for="ÓB">ÓB</label>
+
+      <input type="radio" id="Skeljungur" value="Skeljungur" v-model="stations">
+      <label for="Skeljungur">Skeljungur</label>
 
 
 
-      <div class="box" v-for="petrol in sortedStations">
+      <div class="box" v-for="petrol in filterStations">
         <h3 class="title is-4">{{ petrol.company }}</h3>
         <h4 class="title is-6">Staðsetning: {{ petrol.name }}</h4>
         <p>Bensín verð {{ petrol.bensin95 }}kr.</p>
@@ -36,15 +64,14 @@ export default {
     },
     filterStations() {
       var self = this;
-      var filterd self.stations.filter(function(station){
-        if(station === petrol.company){
-          return true;
+      return this.sortedStations.filter(function(item) {
+        if(self.stations === "Allt"){
+          return item.company;
         }
-        else(station !== petrol.company){
-          return false;
+        else{
+          return item.company === self.stations;
         }
-      });
-
+      })
     }
 
 
