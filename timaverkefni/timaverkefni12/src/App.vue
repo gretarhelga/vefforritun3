4ef9e4dd5e8999accc3af8a73c8416f45be6a899
gretar <gretar95@gmail.com>
2017-11-25 02:11:05 +0000

    heimaverkefni 3 og 4

--- timaverkefni/timaverkefni12/src/App.vue
+++ timaverkefni/timaverkefni12/src/App.vue
@@ -1,44 +1,46 @@
 <template>
   <div id="app">
-    <div class="column is-6 is-offset-3">
+    <div class="column is-4 is-offset-4">
+      <div class="filter">
+        <input type="radio" id="Allt" value="Allt" v-model="stations">
+        <label for="Allt">Allt</label>
 
-      <input type="radio" id="Allt" value="Allt" v-model="stations">
-      <label for="Allt">Allt</label>
+        <input type="radio" id="Atlantsolía" value="Atlantsolía" v-model="stations">
+        <label for="Atlantsolía">Atlantsolía</label>
 
-      <input type="radio" id="Atlantsolía" value="Atlantsolía" v-model="stations">
-      <label for="Atlantsolía">Atlantsolía</label>
+        <input type="radio" id="Costco Iceland" value="Costco Iceland" v-model="stations">
+        <label for="Costco Iceland">Costco Iceland</label>
 
-      <input type="radio" id="Costco Iceland" value="Costco Iceland" v-model="stations">
-      <label for="Costco Iceland">Costco Iceland</label>
+        <input type="radio" id="Dælan" value="Dælan" v-model="stations">
+        <label for="Dælan">Dælan</label>
 
-      <input type="radio" id="Dælan" value="Dælan" v-model="stations">
-      <label for="Dælan">Dælan</label>
+        <input type="radio" id="N1" value="N1" v-model="stations">
+        <label for="N1">N1</label>
 
-      <input type="radio" id="N1" value="N1" v-model="stations">
-      <label for="N1">N1</label>
+        <input type="radio" id="Olís" value="Olís" v-model="stations">
+        <label for="Olís">Olís</label><br>
 
-      <input type="radio" id="Olís" value="Olís" v-model="stations">
-      <label for="Olís">Olís</label>
+        <input type="radio" id="Orkan" value="Orkan" v-model="stations">
+        <label for="Orkan">Orkan</label>
 
-      <input type="radio" id="Orkan" value="Orkan" v-model="stations">
-      <label for="Orkan">Orkan</label>
+        <input type="radio" id="Orkan X" value="Orkan X" v-model="stations">
+        <label for="Orkan X">Orkan X</label>
 
-      <input type="radio" id="Orkan X" value="Orkan X" v-model="stations">
-      <label for="Orkan X">Orkan X</label>
+        <input type="radio" id="ÓB" value="ÓB" v-model="stations">
+        <label for="ÓB">ÓB</label>
 
-      <input type="radio" id="ÓB" value="ÓB" v-model="stations">
-      <label for="ÓB">ÓB</label>
-
-      <input type="radio" id="Skeljungur" value="Skeljungur" v-model="stations">
-      <label for="Skeljungur">Skeljungur</label>
+        <input type="radio" id="Skeljungur" value="Skeljungur" v-model="stations">
+        <label for="Skeljungur">Skeljungur</label>
+      </div>
 
 
+      <p style="padding:15px;">Fjöldi Stöðva: {{ filterStations.length }}</p>
 
       <div class="box" v-for="petrol in filterStations">
-        <h3 class="title is-4">{{ petrol.company }}</h3>
-        <h4 class="title is-6">Staðsetning: {{ petrol.name }}</h4>
-        <p>Bensín verð {{ petrol.bensin95 }}kr.</p>
-        <p>Dísel verð {{ petrol.diesel }}kr.</p>
+        <h3 class="title is-4" >{{ petrol.company }}</h3>
+        <h4 class="title is-6">{{ petrol.name }}</h4>
+        <p class="bensin">Bensín - {{ petrol.bensin95 }}</p>
+        <p class="disel">Dísel - {{ petrol.diesel }}</p>
       </div>
     </div>
   </div>
@@ -52,7 +54,7 @@ export default {
   data () {
     return {
       petrols: [],
-      stations: []
+      stations: "Allt"
     };
   },
   computed: {
@@ -91,5 +93,20 @@ export default {
 </script>
 
 <style> 
-
+.filter{
+  padding: 30px;
+  text-align: center;
+}
+.bensin{
+  position: absolute;
+  margin-left: 450px;
+  margin-top: -90px;
+  color: #D8D8D8;
+}
+.disel{
+  position: absolute;
+  margin-left: 462px;
+  margin-top: -50px;
+  color: #D8D8D8;
+}
 </style>
